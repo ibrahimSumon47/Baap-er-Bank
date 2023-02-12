4575fe5b65cfdd8d327d169bb9f924057b6457a3
ibrahimSumon47 <ibrahimsumon005@gmail.com>
2023-02-12 15:18:14 +0600

    bank added

--- js/login.js
+++ js/login.js
@@ -4,5 +4,10 @@ document.getElementById("btn-login").addEventListener("click",function(){
     const email =emailField.value;
     const passwordField = document.getElementById("user-password");
     const password = passwordField.value;
-    
+    if(email === "ibrahimsumon005@gmail.com" && password ==="AbcD1234~!@#"){
+        window.location.href = "bank.html";
+    }
+    else{
+        alert("invalid User");
+    }
 })
